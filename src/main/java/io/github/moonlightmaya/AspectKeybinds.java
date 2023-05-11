6f3b77a803a765659231e5e304154eb983523af7
Moonlight-Maya <83429328+Moonlight-Maya@users.noreply.github.com>
2023-05-11 13:11:20 -0400

    Close GUI when hitting escape

--- src/main/java/io/github/moonlightmaya/AspectKeybinds.java
+++ src/main/java/io/github/moonlightmaya/AspectKeybinds.java
@@ -4,7 +4,12 @@ import io.github.moonlightmaya.manage.AspectManager;
 import io.github.moonlightmaya.script.events.EventHandler;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
+import net.minecraft.client.Keyboard;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.input.Input;
 import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
 
 public class AspectKeybinds {
@@ -12,12 +17,13 @@ public class AspectKeybinds {
     private static final String CATEGORY = "key.category.aspect";
 
     public static final KeyBinding MENU;
-    private static boolean menuOpen;
+    public static boolean menuOpen;
 
     static {
         MENU = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.aspect.menu", GLFW.GLFW_KEY_UNKNOWN, CATEGORY));
 
         //Menu
+        //NOTE: Menu is also managed by KeyboardMixin.java! (Handling the ESC key to close)
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             while (MENU.wasPressed()) {
                 Aspect guiAspect = AspectManager.getGuiAspect();
---
+++ src/main/java/io/github/moonlightmaya/mixin/input/KeyboardMixin.java
@@ -0,0 +1,43 @@
+package io.github.moonlightmaya.mixin.input;
+
+import io.github.moonlightmaya.Aspect;
+import io.github.moonlightmaya.AspectKeybinds;
+import io.github.moonlightmaya.manage.AspectManager;
+import io.github.moonlightmaya.script.events.EventHandler;
+import net.minecraft.client.Keyboard;
+import net.minecraft.client.MinecraftClient;
+import org.lwjgl.glfw.GLFW;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Keyboard.class)
+public class KeyboardMixin {
+
+
+    @Shadow @Final private MinecraftClient client;
+
+    /**
+     * If the escape key gets pressed, and we're inside a GUI aspect, then we want to
+     * close the GUI in the same way that regular Minecraft screens are closed.
+     */
+
+    @Inject(method = "onKey", at = @At("HEAD"), cancellable = true)
+    public void interceptEscapeKeyForAspectGui(long window, int key, int scancode, int action, int modifiers, CallbackInfo ci) {
+        if (window != client.getWindow().getHandle()) return;
+
+        if (key == GLFW.GLFW_KEY_ESCAPE && client.currentScreen == null && AspectKeybinds.menuOpen) {
+            Aspect guiAspect = AspectManager.getGuiAspect();
+            if (guiAspect != null && guiAspect.scriptHandler != null) {
+                AspectKeybinds.menuOpen = false;
+                guiAspect.scriptHandler.callEvent(EventHandler.GUI_CLOSE);
+            }
+            ci.cancel();
+        }
+    }
+
+
+}
